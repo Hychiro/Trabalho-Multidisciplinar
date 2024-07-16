5d2c33db5b058660bce29b81ea4ec2309e54350f
Hychiro <hierohenrique@gmail.com>
2024-07-16 20:30:32 -0300

    add contorno

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import numpy as np
 import math
-import matplotlib as plt
-from codigo import fw_,krg_,krw_, lambg_,lambw_,phi_,swe_,PorositiyProblem
+import matplotlib.pyplot as plt
+from codigo import PorositiyProblem
 
+if __name__ == '__main__':
+    pp = PorositiyProblem()
+    pp.settingEnviroment()
 
-
+    # plt.matshow(pp.Sw)
+    # plt.show()
+    solcmep = np.arange(0,1.001,0.001)
+    pp.calculate()
+    for i in range(0,len(pp.sol_tempo)):
+        plt.matshow(pp.sol_tempo2[i] )
+        plt.show()
 
 
