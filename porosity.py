0e41516d125e406f9b0e34681147861f4c9ce0a5
Hychiro <hierohenrique@gmail.com>
2024-07-18 22:44:55 -0300

    working

--- porosity.py
+++ porosity.py
@@ -68,6 +68,17 @@ def Fw(S_past):
     ## COLOCAR AQUI O CODIGO PARA CALCULAR FW
     return old_F.copy()
 
+
+def vel_Field(S_past,k):
+    muw = 1.0 # densidade da agua em g/ml
+    vx = np.zeros_like(S_past)
+    vy = np.zeros_like(S_past)
+    for i in range(1, S_past.shape[0]-1):
+        for j in range(1, S_past.shape[1]-1):
+            vx[i,j] = (-k/muw)*(S_past[i+1, j] - S_past[i-1, j]) / (2*dx)
+            vy[i,j] =  (-k/muw)*(S_past[i, j+1] - S_past[i, j-1]) / (2*dy)
+    return vx.copy(),vy.copy()
+
 def up_S(S_past, k, vx, vy, dt, dx, dy, r):
     old_F = Fw(S_past)
     S_new = np.zeros_like(S_past)
@@ -78,29 +89,43 @@ def up_S(S_past, k, vx, vy, dt, dx, dy, r):
                 d2S_dy2 = (S_past[i, j+1] - 2*S_past[i, j] + S_past[i, j-1]) / (dy**2)
                 dF_dx = (old_F[i+1, j] - old_F[i-1, j]) / (2*dx)
                 dF_dy = (old_F[i, j+1] - old_F[i, j-1]) / (2*dy)
-
+                
                 if valid_vortex(S_past, dx, dy, i, j, r):
-                    S_new[i, j] = k * (d2S_dx2 + d2S_dy2) - vx * dF_dx - vy * dF_dy
+                    
+                    S_new[i, j] = k * (d2S_dx2 + d2S_dy2) - vx[i,j] * dF_dx - vy[i,j] * dF_dy
                     S_new[i, j] = S_past[i, j] + S_new[i, j] * dt
     return S_new
 
-max_T, max_X, max_Y, r = 500, 100, 100, 40
-dt, dx, dy = 1, 1, 1
-vx, vy = 0.012, 0.012
-k = 0.1
+max_T, max_X, max_Y, r = 10000, 113, 113, 56.5
+dt, dx, dy = 0.5, 1, 1
+# vx, vy = 0.012, 0.012
+k = 0.3302369
 
 S = np.zeros((int(max_T/dt), int(max_X/dx), int(max_Y/dy)))
-S[0, 40:60, 15:40] = 1
+# S[0, 40:60, 15:40] = 1
+# S[0, 56, 1] = 1
+S[0,50:60,1:5] = 1
+# S[0,50:60,1:5] = 0.1
+
+q = 2.0979020979e-6
+A = 0.00049087385
+valV = (q*k)/A
 
 for t in range(1, S.shape[0]):
+    vx,vy = vel_Field(S[t-1],k)
+    vx[50:60,1:5] = valV
+    vy[50:60,107:112] = valV
     S[t-1] = set_border(S[t-1], dx, dy, r)
     S[t] = up_S(S[t-1], k, vx, vy, dt, dx, dy, r)
     S[t-1] = clear_outside(S[t-1], dx, dy, r)
-    S[t, 40:60, 15:40] = 1
+    # S[t, 40:60, 15:40] = 1
+    # S[t, 56, 1] = 1
+    S[t,50:60,1:5] = 1
+    
 
 import imageio
 images = []
-for i in range(S.shape[0]):
+for i in range(0,S.shape[0],100):
     plt.imshow(S[i], cmap='viridis')
     x_center, y_center = S.shape[1]/2, S.shape[2]/2
     circle = plt.Circle((x_center, y_center), r, color='r', fill=False)
